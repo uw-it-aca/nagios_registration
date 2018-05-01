95434d21e4d03d31f587878fccd9524cf20abfd2
k24dizzle <zhaok24@uw.edu>
2018-05-01 16:08:05 -0700

    Style and first attempt at adding service accordions

--- nagios_registration/static/js/search.js
+++ nagios_registration/static/js/search.js
@@ -37,6 +37,26 @@ $("#delHostModal").on("show.bs.modal", function(event) {
     });
 });
 
+var acc = $(".accordion");
+console.log(acc);
+console.log(acc[0]);
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
 //
 $("#delHostModal").on("hide.bs.modal", function() {
     $("#deleteConfirm").off("click");
