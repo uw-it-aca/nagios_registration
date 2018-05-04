2385aa9f552d18c87eb96ea8c2a0fe3dd67c5171
k24dizzle <zhaok24@uw.edu>
2018-05-04 12:21:08 -0700

    Very basic version of deleting a service

--- nagios_registration/static/js/search.js
+++ nagios_registration/static/js/search.js
@@ -14,6 +14,37 @@ $("#delHostModal").modal({
   "show"      : false,
 });
 
+$("#delServiceModal").modal({
+  "backdrop"  : "static",
+  "keyboard"  : true,
+  "show"      : false,
+});
+
+$("#delServiceModal").on("show.bs.modal", function(event) {
+    var trigger = $(event.relatedTarget);
+    var host = trigger.data('host');
+    var service = trigger.data('service');
+    var modal = $(this);
+    modal.find('.modal-title').text('Are you sure you want to delete: ' + service + ' from ' + host + '?');
+    console.log("service");
+
+    $("#delServiceConfirm").on("click", function(e) {
+        console.log("DELETING SERVICE");
+        // Ajax call here
+        $.ajax({
+            url: "/api/v1/service/" + host + "/" + service,
+            type: 'DELETE',
+            success: function(data) {
+                // Load in the data again (refresh the hosts)
+                loadData();
+            }
+        }).fail(function(data) {
+            alert("FAILURE");
+        });
+        $("#delServiceModal").modal('hide');
+    });
+});
+
 
 $("#delHostModal").on("show.bs.modal", function(event) {
     var trigger = $(event.relatedTarget);
